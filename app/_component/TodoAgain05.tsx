a15a0b49bdff3747ef441ac7a7809aa69aa45b28
sforsanjnarao <sanjnarao26@gmail.com>
2026-02-14 13:07:42 +0530

    Add TodoAgain05 and useLoading components

---
+++ app/_component/TodoAgain05.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TodoAgain05 = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default TodoAgain05
---
+++ app/_component/useLoading.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+//what's the use-case of this
+const useLoading = () => {
+  
+}
+
+export default useLoading
